f94313d9da7f397eba5c8ffae1197b4a83051613
Daniel Narey <danielnarey@users.noreply.github.com>
2020-03-31 23:28:43 -0400

    sync

---
+++ src/app/_Footer/Footer.svelte
@@ -0,0 +1,18 @@
+<script>
+  import content from './content.md';
+  
+</script>
+
+
+<template lang="pug">
+  section
+    != '{content}'
+
+</template>
+
+
+<style lang="postcss">
+  section
+    @apply text-center
+
+</style>
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,8 +4,13 @@ import { createBrowserHistory } from 'history';
 import startTabRouter from './start-tab-router';
 import routeMap from '../.metamodern/route-map';
 
+import Footer from '../app/_Footer/Footer.svelte';
 
 try {
+  const footer = new Footer({
+    target: document.getElementById('footerContent'),
+  });
+
   const history = createBrowserHistory();
 
   startTabRouter(history, routeMap);
