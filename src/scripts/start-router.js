1066343dc2907fab4fb29084667aa117ec805268
Daniel Narey <danielnarey@users.noreply.github.com>
2020-04-03 22:25:01 -0400

    sync

--- src/scripts/start-router.js
+++ src/scripts/start-router.js
@@ -3,12 +3,16 @@ import navaid from 'navaid';
 
 const startRouter = (target, routes) => {
   const router = navaid();
+  let current = new Map(routes).get('/');
 
   routes.forEach(([path, component]) => {
     router.on(path, () => {
-      new component({
+      current.$destroy();
+
+      current = new component({
         target: document.getElementById(target),
       });
+
       console.log(`loaded: ${path}`);
     });
   });
