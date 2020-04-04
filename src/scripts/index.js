133b2bf95e246fcf2587df72ac258a0c9e15db60
Daniel Narey <danielnarey@users.noreply.github.com>
2020-04-04 18:13:17 -0400

    sync

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,18 +3,10 @@
 import App from '../app/App.svelte';
 
 
-
 try {
   new App({
     target: document.body,
   });
-  
-  startRouter('pageContent', [
-    ['/', About],
-    ['/projects', Projects],
-    ['/uses', Uses],
-    ['/now', Now],
-  ]);
 } catch (err) {
   console.log(err);
 }
