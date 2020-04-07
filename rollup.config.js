80558ad10144dfeb326d20b0da57c31d8740ad34
Daniel Narey <danielnarey@users.noreply.github.com>
2020-04-07 00:30:27 -0400

    sync

--- rollup.config.js
+++ rollup.config.js
@@ -53,7 +53,7 @@ export default {
         dev,
         preprocess,
         hydratable: true,
-        css: (css) => { css.write('./__sapper__/export/global.css', false); },
+        css: (css) => { css.write('./static/global.css', false); },
       }),
       resolve({
         browser: true,
