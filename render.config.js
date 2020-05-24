3a85063336ae488e4c733b840aa70e750534e1ba
Daniel Narey <danielnarey@users.noreply.github.com>
2020-05-24 00:27:05 -0400

    sync

--- render.config.js
+++ render.config.js
@@ -1,9 +1,11 @@
+import path from 'path';
+
 import sveltePreprocess from 'svelte-preprocess';
 import markdown from '@jackfranklin/rollup-plugin-markdown';
 import sugarss from 'sugarss';
 import tailwindcss from 'tailwindcss';
 import designSystem from '@metamodern/design-system';
-
+import copyTypefaces from '@metamodern/copy-typefaces';
 
 
 const config = {
@@ -18,6 +20,7 @@ const config = {
   rollupInputPlugins: [
     markdown(),
   ],
+  onRender: () => copyTypefaces(path.resolve(__dirname)),
 };
 
 
