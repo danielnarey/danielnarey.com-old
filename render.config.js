29d4dfaae410bc5a7e4e0657503950f753f8be3f
Daniel Narey <danielnarey@users.noreply.github.com>
2020-05-24 01:06:41 -0400

    sync

--- render.config.js
+++ render.config.js
@@ -1,6 +1,7 @@
 import sveltePreprocess from 'svelte-preprocess';
 import markdown from '@jackfranklin/rollup-plugin-markdown';
 import sugarss from 'sugarss';
+import postcssEasyImport from 'postcss-easy-import';
 import tailwindcss from 'tailwindcss';
 import designSystem from '@metamodern/design-system';
 import copyTypefaces from '@metamodern/copy-typefaces';
@@ -11,6 +12,7 @@ const config = {
     postcss: {
       parser: sugarss,
       plugins: [
+        postcssEasyImport({ extensions: ['.css', '.sss'] }),
         tailwindcss(designSystem({ rotation: -15 })),
       ],
     },
